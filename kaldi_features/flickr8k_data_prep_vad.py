b0ac6687e5191165b7ac1538b321956ab52a8910
Herman Kamper <kamperh@gmail.com>
2017-07-14 13:41:52 +0200

    Small updates to readme and Kaldi features

--- kaldi_features/flickr8k_data_prep_vad.py
+++ kaldi_features/flickr8k_data_prep_vad.py
@@ -11,6 +11,7 @@ Contact: kamperh@gmail.com
 Date: 2017
 """
 
+from __future__ import print_function
 from datetime import datetime
 from os import path
 import glob
@@ -48,13 +49,13 @@ def ctm_to_dict(ctm_fn):
 
 def main():
 
-    print datetime.now()
+    print(datetime.now())
 
     # Directories
     if not path.isdir(data_dir):
         os.makedirs(data_dir)
 
-    print "Reading:", ctm_fn
+    print("Reading:", ctm_fn)
     ctm_dict = ctm_to_dict(ctm_fn)
 
     # Speaker to utterance and vice versa
@@ -78,25 +79,25 @@ def main():
             utt_to_spk_dict[utt] = speaker
             speakers.add(speaker)
             utterances.append(utt)
-    print "No. speakers:", len(speakers)
+    print("No. speakers:", len(speakers))
     spk_to_utt_fn = path.join(data_dir, "spk2utt")
-    print "Writing:", spk_to_utt_fn
+    print("Writing:", spk_to_utt_fn)
     with open(spk_to_utt_fn, "w") as f:
         for speaker in sorted(spk_to_utt_dict):
             f.write(speaker + " " + " ".join(sorted(spk_to_utt_dict[speaker])) + "\n")
     utt_to_spk_fn = path.join(data_dir, "utt2spk")
-    print "Writing:", utt_to_spk_fn
+    print("Writing:", utt_to_spk_fn)
     with open(utt_to_spk_fn, "w") as f:
         for utt in sorted(utt_to_spk_dict):
             f.write(utt + " " + utt_to_spk_dict[utt] + "\n")
     speakers_fn = path.join(data_dir, "speakers")
-    print "Writing:", speakers_fn
+    print("Writing:", speakers_fn)
     with open(speakers_fn, "w") as f:
         for speaker in sorted(list(speakers)):
             f.write(speaker + "\n")
 
     wav_scp_fn = path.join(data_dir, "wav.scp")
-    print "Writing:", wav_scp_fn
+    print("Writing:", wav_scp_fn)
     with open(wav_scp_fn, "w") as f:
         for utt in sorted(utterances):
             wav = path.join(wav_dir, utt[4:] + ".wav")
@@ -105,8 +106,8 @@ def main():
     # Segments and text
     segments_fn = path.join(data_dir, "segments")
     text_fn = path.join(data_dir, "text")
-    print "Writing:", segments_fn
-    print "Writing:", text_fn
+    print("Writing:", segments_fn)
+    print("Writing:", text_fn)
     total_duration = 0
     with open(segments_fn, "w") as segments_f, open(text_fn, "w") as text_f:
         for utt in sorted(utterances):
@@ -120,10 +121,10 @@ def main():
             segments_f.write("{} {} {:.2f} {:.2f}\n".format(utt, utt, float(start), float(end)))
             text_f.write(utt + " " + " ".join([i[2].lower() for i in ctm_entry]) + "\n")
             total_duration += float(end) - float(start)
-    print "No. alignments missing from CTM:", n_missing
-    print "Total duration: {:.2f} hours".format(total_duration/60.0/60.0)
+    print("No. alignments missing from CTM:", n_missing)
+    print("Total duration: {:.2f} hours".format(total_duration/60.0/60.0))
 
-    print datetime.now()
+    print(datetime.now())
 
 
 if __name__ == "__main__":
